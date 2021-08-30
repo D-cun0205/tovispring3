1ee10854c96d0fbe02f40c1d3f6a2b7efd763555
sanghun park <mmeo0205@gmail.com>
2021-08-30 18:16:19 +0900

    6장 AOP 516페이지 6.6.2 트랜잭션 인터셉터와 트랜잭션 속성

--- src/test/java/UserServiceImplTest.java
+++ src/test/java/UserServiceImplTest.java
@@ -141,12 +141,26 @@ public class UserServiceImplTest extends UserServiceImpl {
                 pointcut.getMethodMatcher().matches(Target.class.getMethod("method"), null), is(false));
     }
 
+    @Test
+    public void pointcut() throws Exception {
+        targetClassPointcutMatches("execution(* *(..))", true, true, true, true, true, true);
+    }
+
     public void pointcutMatches(String expression, boolean expected, Class<?> clazz, String methodName, Class<?>... args) throws Exception {
         AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
         pointcut.setExpression(expression);
 
         assertThat(pointcut.getClassFilter().matches(clazz) &&
                 pointcut.getMethodMatcher().matches(clazz.getMethod(methodName, args), null), is(expected));
+    }
+
+    public void targetClassPointcutMatches(String expression, boolean... expected) throws Exception {
+        pointcutMatches(expression, expected[0], Target.class, "hello");
+        pointcutMatches(expression, expected[1], Target.class, "hello", String.class);
+        pointcutMatches(expression, expected[2], Target.class, "plus", int.class, int.class);
+        pointcutMatches(expression, expected[3], Target.class, "minus", int.class, int.class);
+        pointcutMatches(expression, expected[4], Target.class, "method");
+        pointcutMatches(expression, expected[5], Bean.class, "method");
     }
 
 // Spring Framework에서 권하는 단위테스트에 필요한 mokito 툴 사용법 6장 초반부에서 다루고 다시 사용하지않으므로 주석처리
