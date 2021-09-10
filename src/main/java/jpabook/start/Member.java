7c9e45fdff40f1fc15a25eedf45e8a19b1850478
sanghun park <mmeo0205@gmail.com>
2021-09-10 14:02:25 +0900

    JPA 128장

--- src/main/java/jpabook/start/Member.java
+++ src/main/java/jpabook/start/Member.java
@@ -1,6 +1,7 @@
 package jpabook.start;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Table(name = "MEMBER")
@@ -27,6 +28,19 @@ public class Member {
     //매핑정보없음
     private Integer age; //나이
 
+    //4장에서 추가
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob
+    private String description;
+
     public String getId() {
         return id;
     }
@@ -49,5 +63,37 @@ public class Member {
 
     public void setAge(Integer age) {
         this.age = age;
+    }
+
+    public RoleType getRoleType() {
+        return roleType;
+    }
+
+    public void setRoleType(RoleType roleType) {
+        this.roleType = roleType;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
