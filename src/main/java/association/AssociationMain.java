8483c7c186c4d5884bb0c69bdc3f4c826cabc8f5
sanghun park <mmeo0205@gmail.com>
2021-09-13 19:52:05 +0900

    JPA 183장

--- src/main/java/association/AssociationMain.java
+++ src/main/java/association/AssociationMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class AssociationMain {
 
@@ -12,6 +13,7 @@ public class AssociationMain {
     static void testSave(EntityManager em) {
         Team team = new Team("team1", "팀1");
         em.persist(team);
+
 
         Member member1 = new Member("member1", "회원1");
         member1.setTeam(team);
@@ -22,12 +24,64 @@ public class AssociationMain {
         em.persist(member2);
     }
 
+    static void testGet(EntityManager em) {
+        Team team = new Team("team1", "팀1");
+        em.persist(team);
+
+        Member member1 = new Member("member1", "회원1");
+        member1.setTeam(team);
+        em.persist(member1);
+
+        Member member3 = em.find(Member.class, "member1");
+        Team team3 = member3.getTeam();
+        System.out.println(team3.getName());
+    }
+
+    static void queryLogicJoin(EntityManager em) {
+        String jpql = "SELECT m FROM Member m JOIN m.team t WHERE t.name = :teamName";
+        List<Member> resultList = em.createQuery(jpql, Member.class).setParameter("teamName", "팀1").getResultList();
+        for(Member member : resultList) {
+            System.out.println(member.getUsername());
+        }
+    }
+
+    static void updateRelation(EntityManager em) {
+        Team team = new Team("team2", "팀2");
+        em.persist(team);
+
+        Member member = em.find(Member.class, "member1");
+        member.setTeam(team);
+    }
+
+    static void deleteRelation(EntityManager em) {
+        Member member1 = em.find(Member.class, "member2");
+        member1.setTeam(null);
+
+        Team team = em.find(Team.class, "team2");
+        em.remove(team);
+    }
+
+    static void biDirection(EntityManager em) {
+        Team team = em.find(Team.class, "team1");
+        List<Member> members = team.getMembers();
+
+        for(Member member : members) {
+            System.out.println(member.getUsername());
+        }
+
+    }
+
     public static void main(String[] args) {
         EntityManager em = emf.createEntityManager();
         EntityTransaction transaction = em.getTransaction();
 
         transaction.begin();
-        testSave(em);
+        //testSave(em);
+        //testGet(em);
+        //queryLogicJoin(em);
+        //updateRelation(em);
+        //deleteRelation(em);
+        biDirection(em);
         transaction.commit();
         em.close();
         emf.close();
--- src/main/java/association/Team.java
+++ src/main/java/association/Team.java
@@ -3,6 +3,9 @@ package association;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Team {
@@ -20,6 +23,9 @@ public class Team {
 
     public Team() {}
 
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
+
     public String getId() {
         return id;
     }
@@ -34,5 +40,13 @@ public class Team {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<Member> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<Member> members) {
+        this.members = members;
     }
 }
