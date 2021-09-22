329e715209f643260b64849ff2fe5a81bbd40187
D-cun0205 <mmeo0205@gmail.com>
2021-09-23 07:56:53 +0900

    334장

---
+++ src/main/java/valuetype/ValueTypeMain.java
@@ -0,0 +1,24 @@
+package valuetype;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class ValueTypeMain {
+
+    static EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpabook");
+
+
+
+    public static void main(String[] args) {
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction et = em.getTransaction();
+
+        et.begin();
+
+        et.commit();
+        em.close();
+        emf.close();
+    }
+}
