3525cea5bdfb6b63f971dd3ce449ae8c546505b3
sanghun park <mmeo0205@gmail.com>
2021-10-05 19:50:53 +0900

    JPA 421장 네이티브 함수 호출

--- src/main/java/ooql/ObjectOQLMain.java
+++ src/main/java/ooql/ObjectOQLMain.java
@@ -1,144 +1,96 @@
 package ooql;
 
-import proxy.Team;
-
-import javax.persistence.*;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 import javax.persistence.criteria.*;
-import java.lang.reflect.Type;
 import java.util.List;
 
 public class ObjectOQLMain {
 
     static EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpabook");
 
-    static void getUser(EntityManager em) {
-        //criteria 사용준비
+    // ge : >=, gt : >, le : <=, lt : <
+
+    //서브쿼리
+    static void subQuery(EntityManager em) {
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Member> mainQuery = cb.createQuery(Member.class);
+
+        Subquery<Double> subQuery = mainQuery.subquery(Double.class);
+        Root<Member> m2 = subQuery.from(Member.class);
+        subQuery.select(cb.avg(m2.<Integer>get("age")));
+
+        Root<Member> m = mainQuery.from(Member.class);
+
+        //select * from Member m where  m.age >= (subquery)
+        //서브쿼리 사용하고 싶을 경우 Root, Join을 통한 별칭으로 접근
+        mainQuery.select(m).where(cb.ge(m.<Integer>get("age"), subQuery));
+    }
+
+    //상호 관계
+    static void mutualRelationship(EntityManager em) {
+        //exists : 값이 존재하는지
+        //select m from Member m where exists (select t from m.team t where t.name = '팀A')
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Member> mainQuery = cb.createQuery(Member.class);
+        Root<Member> m = mainQuery.from(Member.class);
+
+        Subquery<Team> subQuery = mainQuery.subquery(Team.class);
+        //서브쿼리에서 메인쿼리 정보를 사용하고 싶은경우 메인쿼리에서 적용한 별칭을 이용하여 적용
+        //메인쿼리의 별칭을 가져옴
+        Root<Member> subM = subQuery.correlate(m);
+
+        Join<Member,Team> t = subM.join("team");
+        subQuery.select(t).where(cb.equal(t.get("name"), "팀A"));
+
+        mainQuery.select(m).where(cb.exists(subQuery));
+        List<Member> resultList = em.createQuery(mainQuery).getResultList();
+    }
+
+    //in절
+    static void inQuery(EntityManager em) {
+        //select m from Member m where m.username in ('회원1', '회원2')
         CriteriaBuilder cb = em.getCriteriaBuilder();
         CriteriaQuery<Member> query = cb.createQuery(Member.class);
+        Root<Member> m = query.from(Member.class);
+        query.select(m).where(cb.in(m.get("username")).value("회원1").value("회원2"));
+    }
 
-        //루트클래스(조회를 시작할 클래스)
+    //case절
+    static void caseQuery(EntityManager em) {
+        //select
+        //  m.username
+        //  , case when m.age >= 60 then 600
+        //          when m.age <= 15 then 500
+        //          else 1000
+        //from Member m
+        //
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Member> query = cb.createQuery(Member.class);
         Root<Member> m = query.from(Member.class);
 
-        String name = "클라이언트 요청에 의해 넘어온 유저 명";
-        CriteriaQuery<Member> cq = query.select(m).where(cb.equal(m.get("username"), name));
-        List<Member> resultList = em.createQuery(cq).getResultList();
-
-//        String jpql = "select o.member.team from order o where o.product.name = 'productA' and a.address.city = 'JINJU'";
-//        String jpql = "select m from Member as m where m.username = 'kim'";
-//        List<Member> resultList = em.createQuery(jpql, Member.class).getResultList();
-    }
-
-    //엔티티 직접 사용 (기본 키)
-    static void getInfo(EntityManager em) {
-        Member member = new Member();
-        //작성한 JPQL은 파라미터로 엔티티를 받는다
-        String sqlString = "select m from Member m where m = :member";
-        //실제 SQL에서는 Member 엔티티의 id값으로 조회
-        //select m.* from Member m where m.id = ?
-        List<Member> members = em.createQuery(sqlString).setParameter("member", member).getResultList();
-        for(Member member1 : members) {
-            System.out.println(member1.getUsername());
-        }
-    }
-
-    //엔티티 직접 사용 (외래 키)
-    static void getInfoForeignKey(EntityManager em) {
-        Team team = em.find(Team.class, 1L);
-        String qlString = "select m from Member m where m.team = :team";
-        List resultList = em.createQuery(qlString).setParameter("team", team).getResultList();
-    }
-
-    static void tte(EntityManager em) {
-        List<Member> resultList = em.createNamedQuery("Member.findByUsername", Member.class).setParameter("username", "회원1").getResultList();
-    }
-
-    //DISTINCT 사용법
-    static void useCriteria(EntityManager em) {
-        //JPQL : SELECT DISTINCT m.username, m.age FROM Member m 아래와 동일
-        CriteriaBuilder cb = em.getCriteriaBuilder();
-        CriteriaQuery<Object[]> cq = cb.createQuery(Object[].class);
-        Root<Member> m = cq.from(Member.class);
-        cq.multiselect(m.get("username"), m.get("age")).distinct(true);
-
-        TypedQuery<Object[]> query = em.createQuery(cq);
-        List<Object[]> list = query.getResultList();
-    }
-
-    //NEW, construct()
-    public void useConstruct(EntityManager em) {
-        //JPQL : SELECT new jpabook.domain.MemberDTO(m.username, m.age) from Member m
-        CriteriaBuilder cb = em.getCriteriaBuilder();
-        CriteriaQuery<Member> cq = cb.createQuery(Member.class);
-        Root<Member> m = cq.from(Member.class);
-        cq.select(cb.construct(Member.class, m.get("username"), m.get("age")));
-
-        TypedQuery<Member> query = em.createQuery(cq);
-        List<Member> list = query.getResultList();
-    }
-
-    //Tuple
-    public void useTuple(EntityManager em) {
-        CriteriaBuilder cb = em.getCriteriaBuilder();
-        //CriteriaQuery<Tuple> cqt = cb.createTupleQuery(); 아래와 동일
-        CriteriaQuery<Tuple> cqt = cb.createQuery(Tuple.class);
-        Root<Member> m = cqt.from(Member.class);
-        cqt.multiselect(
-                m.get("username").alias("username"),
-                m.get("age").alias("age")
+        query.multiselect(m.get("username"), cb.selectCase()
+                .when(cb.ge(m.<Integer>get("age"), 60), 600)
+                .when(cb.le(m.<Integer>get("age"), 15), 500)
+                .otherwise(1000)
         );
-
-        TypedQuery<Tuple> query = em.createQuery(cqt);
-        List<Tuple> list = query.getResultList();
-        for(Tuple tuple : list) {
-            String username = tuple.get("username", String.class);
-        }
     }
 
-    //GROUP BY
-    public void useGroupBy(EntityManager em) {
-        //JPQL : SELECT m.team.name, MAX(m.age), MIN(m.age) FROM Member m GROUP BY m.team.name
+    //파라미터정의
+    static void paramManager(EntityManager em) {
         CriteriaBuilder cb = em.getCriteriaBuilder();
-        CriteriaQuery<Object[]> cq = cb.createQuery(Object[].class);
-        Root<Member> m = cq.from(Member.class);
+        CriteriaQuery<Member> query = cb.createQuery(Member.class);
+        Root<Member> m = query.from(Member.class);
 
-        //Expression maxAge = cb.max(m<Integer>.get("age"));
-        Expression<Number> maxAge = cb.max(m.get("age"));
-        Expression<Number> minAge = cb.min(m.get("age"));
-
-        cq.multiselect(m.get("team").get("name"), maxAge, minAge);
-        cq.groupBy(m.get("team").get("name"));
-        //HAVING - 팀에서 가장 나이 어린사람 중 나이가 10을 초과
-        //JPQL : HAVING min(m.age) > 10 와 동일
-        cq.having(cb.gt(minAge, 10));
-        //ORDER BY
-        cq.orderBy(cb.desc(m.get("username")));
-
-        TypedQuery<Object[]> query = em.createQuery(cq);
-        List<Object[]> list = query.getResultList();
+        //cb.parameter 파라미터정의, setParameter("", "") 파라미터바인딩
+        query.select(m).where(cb.equal(m.get("username"), cb.parameter(String.class, "usernameParam")));
+        List<Member> resultList = em.createQuery(query).setParameter("usernameParam", "회원1").getResultList();
     }
 
-    //JOIN
-    public void useJoin(EntityManager em) {
-        //JPQL : SELECT m, t FROM Member m INNER JOIN m.team t WHERE t.name = '팀A';
-        CriteriaBuilder cb = em.getCriteriaBuilder();
-        CriteriaQuery<Object[]> cq = cb.createQuery(Object[].class);
-        Root<Member> m = cq.from(Member.class);
-        Join<Member, ooql.Team> t = m.join("team", JoinType.INNER); //내부조인
-        m.fetch("team", JoinType.LEFT);
-        cq.multiselect(m, t).where(cb.equal(t.get("name"),"팀A"));
-    }
+    static void nativeFun(EntityManager em) {
 
-    //SUB QUERY
-    public void useSubQuery(EntityManager em) {
-        //JPQL : SELECT m FROM Member m WHERE m.age >= (SELECT AVG(m2.age) FROM Member m2)
-        CriteriaBuilder cb = em.getCriteriaBuilder();
-        CriteriaQuery<Member> cq = cb.createQuery(Member.class);
-        Subquery<Double> subquery = cq.subquery(Double.class);
-        Root<Member> m = subquery.from(Member.class);
-        subquery.select(cb.avg(m.get("age")));
-
-        Root<Member> mainM = cq.from(Member.class);
-        cq.select(mainM).where(cb.ge(m.get("age"), subquery));
     }
 
     public static void main(String[] args) {
@@ -146,9 +98,10 @@ public class ObjectOQLMain {
         EntityTransaction transaction = em.getTransaction();
 
         transaction.begin();
-        getUser(em);
+
         transaction.commit();
         em.close();
         emf.close();
     }
+
 }
